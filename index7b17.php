6d33802d33986107b76645704d19e1fa7d7b41fa
VAIBHAV-APO <VAIBHAV-APO>
2017-09-28 16:35:07 +0530

    insure

---
+++ index7b17.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Home</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://themewinter.com"/>
+	<id>http://themewinter.com/joomla/saifway/index.php</id>
+	<updated>2016-07-03T12:31:02+00:00</updated>
+	<generator uri="http://joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://themewinter.com/joomla/saifway/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Home</title>
+		<description><![CDATA[]]></description>
+		<link>http://themewinter.com/joomla/saifway/index.php</link>
+		<lastBuildDate>Sun, 03 Jul 2016 12:31:02 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://themewinter.com/joomla/saifway/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
